8587007729b9eb1b04d7823c6bedfc9c605ded25
Daniel Collins <solemnwarning@solemnwarning.net>
2008-04-21 21:01:44 +0000

    Updated the output of load_kernel()

--- src/kexec.c
+++ src/kexec.c
@@ -86,19 +86,25 @@ int load_kernel(char const* kernel, char const* append, char const* initrd) {
 	argv_append("-l");
 	argv_append((char*)kernel);
 	
+	printd("Loading kernel...");
+	printd("\tkernel: %s", kernel);
+	
 	if(append[0] != '\0') {
 		snprintf(append_arg, STACK_BUF, "--append=%s", append);
 		argv_append(append_arg);
+		
+		printd("\tappend: %s", append);
 	}
 	if(initrd[0] != '\0') {
 		snprintf(initrd_arg, STACK_BUF, "--initrd=%s", initrd);
 		argv_append(initrd_arg);
+		
+		printd("\tinitrd: %s", initrd);
 	}
 	
 	if(run_kexec(kexec_argv) != 0) {
-		printD("run_kexec() returned nonzero");
-		return(0);
+		return 0;
 	}
 	
-	return(1);
+	return 1;
 }
