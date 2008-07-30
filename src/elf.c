0e7a5d6570a1c0a4a90c657dace66e81ca2361c3
Daniel Collins <solemnwarning@solemnwarning.net>
2008-07-30 16:55:49 +0000

    Wrote elf2host() and started implementing elf32_getsection()

--- src/elf.c
+++ src/elf.c
@@ -30,17 +30,31 @@
 
 #include <stdlib.h>
 #include <elf.h>
+#include <stdint.h>
+#include <string.h>
 
 #include "elf.h"
 #include "mystring.h"
 
+#define elf2host(dest, src, size) \
+	elf2host_(dest, src, size, elf[EI_DATA])
+
 static char *elf32_getsection(char const *elf, char const *name, size_t *size);
 static char *elf64_getsection(char const *elf, char const *name, size_t *size);
+static void elf2host_(void *dest, void const *src, size_t size, char eidata);
 
 /* Load a section from an ELF binary
  * Calls elf32_getsection() or elf64_getsection() depending on ELF format
 */
 char *elf_getsection(char const *elf, char const *name, size_t *size) {
+	union {
+		uint16_t i;
+		char c[2];
+	} etest;
+	
+	etest.i = 1;
+	
+	
 	if(elf[0] != 0x7F || !str_eq(elf+1, "ELF", 3)) {
 		return NULL;
 	}
@@ -59,6 +73,14 @@ char *elf_getsection(char const *elf, char const *name, size_t *size) {
  * Returns NULL if the section was not found
 */
 static char *elf32_getsection(char const *elf, char const *name, size_t *size) {
+	Elf32_Off e_shoff;
+	Elf32_Half e_shentsize;
+	Elf32_Half e_shnum;
+	
+	elf2host(&e_shoff, elf+32, sizeof(Elf32_Off));
+	elf2host(&e_shentsize, elf+46, sizeof(Elf32_Half));
+	elf2host(&e_shnum, elf+48, sizeof(Elf32_Half));
+	
 	return NULL;
 }
 
@@ -67,4 +89,29 @@ static char *elf32_getsection(char const *elf, char const *name, size_t *size) {
 */
 static char *elf64_getsection(char const *elf, char const *name, size_t *size) {
 	return NULL;
+}
+
+/* Convert a value in the ELF binary from the ELF endian to the host endian. */
+static void elf2host_(void *dest, void const *src, size_t size, char eidata) {
+	static char host_endian = ELFDATANONE;
+	
+	if(host_endian == ELFDATANONE) {
+		int16_t test = 1;
+		
+		if(((char*)&test)[0] == 1) {
+			host_endian = ELFDATA2LSB;
+		}else{
+			host_endian = ELFDATA2MSB;
+		}
+	}
+	
+	if(host_endian != eidata) {
+		size_t n;
+		
+		for(n = 0; n < size; n++) {
+			((char*)dest)[n] = ((char*)src)[size-n-1];
+		}
+	}else{
+		memcpy(dest, src, size);
+	}
 }
