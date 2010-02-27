0ba4323576239bec8e815f8bbb2d1f7ed2514760
Daniel Collins <solemnwarning@solemnwarning.net>
2010-02-27 19:23:18 +0000

    Rewrote tarmods.pl

--- tarmods.pl
+++ tarmods.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
-# Create TAR packages of kernel modules
-# Copyright (C) 2007-2009 Daniel Collins <solemnwarning@solemnwarning.net>
+# Create a modules package from a kernel tree
+# Copyright (C) 2007-2010 Daniel Collins <solemnwarning@solemnwarning.net>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,155 +19,124 @@
 use strict;
 use warnings;
 
-my %groups = (
-	"ide-pcmcia" => ["ide-cs"],
-	"ide" => ["drivers/ide"],
-	"memstick" => ["drivers/memstick/"],
-	"mmc-sd" => ["drivers/mmc/"],
-	"paride" => ["drivers/block/paride"],
-	"firewire" => ["drivers/ieee1394"],
-	"sata" => ["drivers/ata", "sx8"],
-	"pcmcia" => ["drivers/pcmcia"],
-	"parport" => ["drivers/parport"]
-);
-
-my @ignore = (
-	"jbd",
-	"jbd2",
-	"firmware_class",
-	"zlib_inflate",
-	"cdrom"
-);
-
 if(@ARGV != 1) {
 	print STDERR "Usage: tarmods.pl <output directory>\n";
-	exit(1);
+	exit 1;
 }
 
-my $outdir = $ARGV[0];
-my %mods = ();
-my @dirs = ();
+my $output = $ARGV[0];
 
-sub mod_deps {
+my @modules_list = (
+	# Random crap that isn't directly used by anyone, so don't package
+	#
+	{ expr => "parport.ko" },
+	{ expr => "zlib_inflate.ko" },
+	{ expr => "firmware_class.ko" },
+	{ expr => "jbd.ko" },
+	{ expr => "jbd2.ko" },
+	
+	{ expr => "fs/nls/", dest => "nls/\$modname" },
+	{ expr => "fs/", dest => "fs/\$modname" },
+	
+	{ expr => "drivers/ata/", dest => "sata" },
+	{ expr => "sx8.ko", dest => "sata" },
+	
+	{ expr => "drivers/ide/", dest => "ide" },
+	{ expr => "drivers/scsi/", dest => "scsi/\$modname" },
+	{ expr => "drivers/ieee1394/", dest => "firewire" },
+	{ expr => "drivers/memstick/", dest => "memstick" },
+	{ expr => "drivers/mmc/", dest => "mmc" },
+	{ expr => "drivers/block/paride/", dest => "paride" },
+	{ expr => "drivers/pcmcia/", dest => "pcmcia" }
+);
+
+my %output_dirs = ();
+
+my @modules = `find -iname '*.ko'` or die;
+
+sub get_deps {
 	my ($module) = @_;
-	my @mi_out = qx(/sbin/modinfo $module);
-	my @depends = ();
 	
-	foreach my $line(@mi_out) {
-		chomp($line);
-		my ($name, $value) = split(/:\s+/, $line, 2);
+	my $out = `modinfo -F depends "$module"`;
+	chomp($out);
+	
+	return split(/,/, $out);
+}
+
+sub copy_module {
+	my ($modpath, $dest) = @_;
+	
+	my $modfile = $modpath;
+	$modfile =~ s/.*\///g;
+	
+	my $modname = $modfile;
+	$modname =~ s/\.ko$//;
+	
+	$dest =~ s/\$modname/$modname/g;
+	$output_dirs{$dest} = 1;
+	
+	system("mkdir -p \"$dest\"") == 0 or die;
+	
+	if(!-e "$dest/$modfile") {
+		system("cp \"$modpath\" \"$dest\"") == 0 or die;
+	}
+	
+	my @deps = get_deps($modpath);
+	
+	foreach my $dep(@deps) {
+		my $found = 0;
 		
-		if($name eq "depends") {
-			push(@depends, split(/,/, $value));
-		}
-	}
-	
-	return @depends;
-}
-
-sub exec_cmd {
-	system($_[0]);
-	
-	if($? == -1 || ($? >> 8) != 0) {
-		print STDERR "Failed to execute \'$_[0]\'\n";
-		exit(1);
-	}
-}
-
-sub copy_mod {
-	my ($module, $dir) = @_;
-	
-	if(!defined($mods{$module})) {
-		print("Warning: Unknown module \'$module\'\n");
-		return;
-	}
-	
-	my $file = $mods{$module}->[1];
-	my @deps = mod_deps($file);
-	
-	exec_cmd("cp \"$file\" \"$dir\"");
-	
-	foreach my $mod(@deps) {
-		copy_mod($mod, $dir);
-	}
-}
-
-exec_cmd("rm -rf $outdir/*");
-
-foreach my $file(qx(find -iname '*.ko')) {
-	chomp($file);
-	
-	my $dir = $file;
-	$dir =~ s/\.\///g;
-	$dir =~ s/\/[^\/]*$//;
-	
-	my $mod = $file;
-	$mod =~ s/.*\///g;
-	$mod =~ s/\.ko//;
-	
-	$mods{$mod} = [$dir, $file];
-}
-
-foreach my $mod(keys(%mods)) {
-	if(grep(/^$mod$/, @ignore)) {
-		next;
-	}
-	
-	my $group = $mod;
-	
-	my $dir = $mods{$mod}->[0];
-	my $file = $mods{$mod}->[1];
-	
-	foreach my $key(keys(%groups)) {
-		my $val = $groups{$key};
-		
-		if(grep(/^$dir$/, @$val) || grep(/^$mod$/, @$val)) {
-			$group = $key;
-			last;
-		}
-		
-		foreach my $foo(@$val) {
-			if($foo =~ /\/$/ && $dir =~ /^$foo/) {
-				$group = $key;
-				goto DONE;
+		foreach my $mod(@modules) {
+			chomp($mod);
+			
+			if($mod =~ /\/$dep\.ko$/) {
+				copy_module($mod, $dest);
+				
+				$found = 1;
+				last;
 			}
 		}
-	}
-	
-	DONE:
-	
-	if(!grep(/^$group$/, @dirs)) {
-		push(@dirs, $group);
-	}
-	
-	exec_cmd("mkdir -p \"$outdir/$group/\"");
-	copy_mod($mod, "$outdir/$group");
-}
-
-foreach my $dir(@dirs) {
-	my $fdir = "$outdir/$dir";
-	
-	exec_cmd("tar --format=v7 -C \"$fdir\" -cf \"$fdir.tar\" ./");
-	exec_cmd("rm -rf \"$fdir\"");
-	exec_cmd("lzma -9 \"$fdir.tar\"");
-	exec_cmd("mv \"$fdir.tar.lzma\" \"$fdir.tlz\"");
-}
-
-foreach my $mod(keys(%mods)) {
-	if(-e "$outdir/$mod.tlz" && !defined($groups{$mod})) {
-		my $dir = $mods{$mod}->[0];
 		
-		if($dir eq "fs/nls") {
-			$dir = "nls";
-		}elsif($dir =~ /^fs\//) {
-			$dir = "fs";
-		}elsif($dir =~ /scsi/) {
-			$dir =~ s/^drivers\///;
-		}else{
-			next;
+		die("Cannot find module \'$dep\'") if(!$found);
+	}
+}
+
+foreach my $module(@modules) {
+	chomp($module);
+	
+	my $match = 0;
+	
+	foreach my $m(@modules_list) {
+		if($module =~ /$m->{"expr"}/) {
+			if($m->{"dest"}) {
+				my @dests = split(/,/, $m->{"dest"});
+				
+				foreach my $dest(@dests) {
+					copy_module($module, "$output/$dest");
+				}
+			}
+			
+			$match = 1;
+			last;
 		}
-		
-		exec_cmd("mkdir -p \"$outdir/$dir/\"");
-		exec_cmd("mv \"$outdir/$mod.tlz\" \"$outdir/$dir/\"");
 	}
+	
+	if(!$match) {
+		my $modname = $module;
+		$modname =~ s/.*\///g;
+		$modname =~ s/\.ko$//g;
+		
+		print("Unknown module: \'$module\'; packaging seperately\n");
+		copy_module($module, "$output/$modname");
+	}
+}
+
+foreach my $path(keys(%output_dirs)) {
+	$path =~ s/\/$//;
+	
+	system("tar -cf $path.tar -C $path ./") == 0 or die;
+	system("rm -r $path") == 0 or die;
+	
+	system("lzma -9c $path.tar > $path.tlz") == 0 or die;
+	system("rm $path.tar") == 0 or die;
 }
